5a507f14121e6f44e609db4b27a278c67f94723b
Григорий Стрельцов <gastreltsov1998@gmail.com>
2021-04-09 13:55:44 +0300

    Added commentaries to InputToAddElement method

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -18,28 +18,39 @@ public class MouseController : MonoBehaviour, IInputController
     {
         if (Input.GetMouseButtonDown(0))
         {
+            // Set position to spawn an element.
             Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             position.z = 0;
 
             GameObject spawnedObject = ObjectPooler.Instance.SpawnFromPool(tag, position, Quaternion.identity);
+
+            // Check for spawnedObject exists.
             if (spawnedObject == null)
             {
                 Debug.LogWarning("GameObject from pool with tag " + tag + " doesn't exist.");
                 return;
             }
+
             SpriteRenderer spriteRenderer = spawnedObject.GetComponent<SpriteRenderer>();
+            // Check for spriteRenderer is attached to spawnedObject.
             if (spriteRenderer == null)
             {
                 Debug.LogWarning("No SpriteRenderer component attached to this GameObject.");
                 return;
             }
+
+            // Deactivating the object in order to check for free space firstly.
             spawnedObject.SetActive(false);
-            //size in Units
+            // Getting the sprite size.
             Vector2 itemSize = spriteRenderer.bounds.size;
 
+            // Getting layer's number and creating a layerMask.
             int layerNumber = LayerMask.NameToLayer(layerName);
             int layerMask = 1 << layerNumber;
+
+            // OverlapBox returns Collider2D which intersects created box.
             Collider2D intersection = Physics2D.OverlapBox(position, itemSize, 0f, layerMask);
+            // Check for intersections.
             if (intersection == null)
             {
                 spawnedObject.SetActive(true);
